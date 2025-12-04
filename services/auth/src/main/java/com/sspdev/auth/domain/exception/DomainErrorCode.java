7e14ef3d7ac19e0fe44b1b7227880d8fce2c6304
Sergey Slepnev <slepnev.sp@gmail.com>
2025-12-04 19:46:44 +0800

    feat(auth): add enum with custom domaint error codes to use in domain exceptions

---
+++ services/auth/src/main/java/com/sspdev/auth/domain/exception/DomainErrorCode.java
@@ -0,0 +1,20 @@
+package com.sspdev.auth.domain.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum DomainErrorCode {
+
+    INVALID_CREATE_USER_REQUEST_DATA("domain.INVALID_CREATE_USER_REQUEST_DATA"),
+    INVALID_ID("domain.INVALID_ID"),
+    CONTACT_REQUIRED("domain.CONTACT_REQUIRED"),
+    PASSWORD_REQUIRED("domain.PASSWORD_REQUIRED"),
+    USER_BY_EMAIL_ALREADY_EXISTS("domain.USER_BY_EMAIL_ALREADY_EXISTS"),
+    USER_BY_PHONE_ALREADY_EXISTS("domain.USER_BY_PHONE_ALREADY_EXISTS");
+
+    private final String messageKey;
+
+    DomainErrorCode(String messageKey) {
+        this.messageKey = messageKey;
+    }
+}
